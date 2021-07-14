db327d20e8137ca8ec89fa538939c700d4b3784d
zhuw <738504427@qq.com>
2021-07-14 17:05:42 +0800

    添加toolbar

--- app/src/main/java/com/hnkj/jitpackstudy/MainActivity.kt
+++ app/src/main/java/com/hnkj/jitpackstudy/MainActivity.kt
@@ -2,10 +2,53 @@ package com.hnkj.jitpackstudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    lateinit var toolbar: Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_settings -> {
+            // User chose the "Settings" item, show the app settings UI...
+            Snackbar.make(toolbar, "点击了设置按钮", Snackbar.LENGTH_SHORT).show()
+            true
+        }
+
+        R.id.action_favorite -> {
+            // User chose the "Favorite" action, mark the current item
+            // as a favorite...
+            Snackbar.make(toolbar, "点击了收藏按钮", Snackbar.LENGTH_LONG).
+            setAction("测试") {
+                Toast.makeText(this, "点击了Snackbar设置的事件", Toast.LENGTH_SHORT).show()
+            }.show()
+            true
+        }
+        android.R.id.home -> {
+            finish()
+            true
+        }
+
+        else -> {
+            // If we got here, the user's action was not recognized.
+            // Invoke the superclass to handle it.
+            super.onOptionsItemSelected(item)
+        }
     }
 }
